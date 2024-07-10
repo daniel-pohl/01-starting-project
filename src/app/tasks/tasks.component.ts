62ba586625dd01ed32bcfbb275ab84931bb69233
daniel-pohl <danielpohl@mail.de>
2024-07-10 11:48:55 +0200

    ok2

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,  
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+
+export class UserComponent {
+  
+
+}
