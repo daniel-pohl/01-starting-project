6fa92718d40325fc24de78b004e1c229653011d2
daniel-pohl <danielpohl@mail.de>
2024-09-22 18:59:03 +0200

    37. Exercise: Create a Configurable Component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 })
 
 export class TasksComponent {
-  
+  @Input({ required:true }) name!: string;
+
 
 }
