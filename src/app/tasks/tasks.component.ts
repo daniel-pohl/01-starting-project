bbeeb64b9a945c27af26eecf33776d47b5cac199
daniel-pohl <danielpohl@mail.de>
2024-10-11 16:38:05 +0200

    55. Using the Submitted Data

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -56,5 +57,20 @@ export class TasksComponent {
 
   onCancelAddTask(){
     this.isAddingTask = false;  
+  }
+
+  onAddTask(taskData: NewTaskData){
+    /* get auch mit this.tasks.push(...
+    -das unshift ist eine Methode um das neue objekt
+    nach oben in dem Array zu setzen  */
+
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    })
+    this.isAddingTask = false;
   }
 }
